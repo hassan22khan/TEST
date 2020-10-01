f09da81c9b4a81e65a7ed4ac58b7c3796444a20a
roney17khan@gmail.com <roney17khan@gmail.com>
2020-10-01 17:46:06 +0500

    Web Api

---
+++ StudentData/Model/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using StudentModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData.Model
+{
+    public class StudentContext :DbContext
+    {
+
+        public StudentContext() : base("name=StudentDB")
+        {
+        }
+
+        public  DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentRepository/StudentService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentData.Model;
+using StudentModels;
+
+namespace StudentRepository
+{
+    public class StudentService
+    {
+       
+        public IEnumerable<Student> GetStudents()
+        {
+            using (var _context = new StudentContext())
+            {
+                return _context.Students.ToList();
+                
+            }  
+        }
+
+        public Student GetStudent(int id)
+        {
+            using (var _context = new StudentContext())
+            {
+                return _context.Students.SingleOrDefault(s => s.Id == id);
+                
+            }
+        }
+
+        public void AddStudent(Student student)
+        {
+            using (var _context = new StudentContext())
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+            }         
+        }
+
+        public void DeleteStudent(int id)
+        {
+            using (var _context = new StudentContext())
+            {
+                var studentToDelete = _context.Students.Single(s => s.Id == id);
+                _context.Students.Remove(studentToDelete);
+                _context.SaveChanges();
+            }
+             
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            using (var _context = new StudentContext())
+            {
+                var students = _context.Students.ToList();
+                var studentToUpdate = students.Single(s => s.Id == student.Id);
+                studentToUpdate.Name = student.Name;
+                studentToUpdate.Email = student.Email;
+                studentToUpdate.Phone = student.Phone;
+                studentToUpdate.Password = student.Password;
+                studentToUpdate.ConfirmPassword = student.ConfirmPassword;
+
+                _context.SaveChanges();
+            }
+            
+        }
+
+    }
+}
