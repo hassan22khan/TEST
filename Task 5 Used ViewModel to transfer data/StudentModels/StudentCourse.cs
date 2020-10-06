09248fa37dd7ef344a0c0c23dc4d31fe49e4f9af
roney17khan@gmail.com <roney17khan@gmail.com>
2020-10-06 13:46:22 +0500

    Used the view Model to get post put and delete. Rendered the courses on front end. StudentCourse table is also updated with ajax call.

---
+++ Task 5 Used ViewModel to transfer data/StudentModels/StudentCourse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class StudentCourse
+    {
+        //key for this table will be a composite key including both foreign keys
+
+        [Key]
+        [Column(Order = 1)]                 //order 1 specifies here that StudentId will be placed first in the composite key
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        [Key]
+        [Column(Order = 2)]                 //order 2 specifies here that StudentId will be placed first in the composite key
+        public int CourseId { get; set; }
+
+        public Course Courses { get; set; }
+    }
+}
